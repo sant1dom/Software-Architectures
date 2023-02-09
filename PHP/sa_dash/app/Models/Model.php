12cef019a64b3b9a2ce427e2153fbb825bf2cc3c
Mario <mario123456789mario@gmail.com>
2023-02-09 12:23:23 +0100

    Client & Gateway

--- PHP/sa_dash/app/Models/Model.php
+++ PHP/sa_dash/app/Models/Model.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model as BaseModel;
 
 class Model extends BaseModel
 {
+	public static function isDebug()
+	{
+		return (isset($_GET["debug"]) && $_GET["debug"]== "debug");
+	}
+
 	public static function sendRequest($request)
 	{
 		$url = "http://api_gateway/index.php?";
@@ -18,30 +23,34 @@ class Model extends BaseModel
 		$raw_response = file_get_contents($url);
 
 		$response = json_decode($raw_response);
-		if (isset($_GET["debug"]) && $_GET["debug"]== "debug")
+		if (static::isDebug())
 		{
+			dump($request);
 			dump($raw_response);
 			dump($response);
-			die();
 		}
 
 		if (!is_object($response))
 		{
 			if (is_string($raw_response))
 			{
+				if (static::isDebug()) die("Line: " . __LINE__);
 				throw new ExceptionError($raw_response);
 			}
 
+			if (static::isDebug()) die("Line: " . __LINE__);
 			throw new ExceptionError("Connection with API error");
 		}
 
 		if ($response->code == "UNLOGGED")
 		{
+			if (static::isDebug()) die("Line: " . __LINE__);
 			throw new ExceptionUnlogged($response->message);
 		}
 
 		if ($response->code != "OK")
 		{
+			if (static::isDebug()) die("Line: " . __LINE__);
 			throw new ExceptionError($response->message);
 		}
 
